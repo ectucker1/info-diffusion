e740aa61ef34cf433bb12973cda0dcf249dcebef
Iyanuoluwa <iy@gidigidiolu.com>
2019-08-24 15:12:03 +0100

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='indiff',
     packages=find_packages(),
-    version='0.2.0',
+    version='0.2.1',
     description='Predict the temporal dynamics of information diffusion on '
     'Twitter',
     author='GidiGidiOLU',
